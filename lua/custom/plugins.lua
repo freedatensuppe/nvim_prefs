f00c3ef66c2351007b0cc78186bd44758df68f20
freedatensuppe <seraphyn.pixel@gmail.com>
2024-04-08 18:52:33 +0200

    vimtex inverse search with zathura now fully functional

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -6,13 +6,9 @@ local plugins = {
   },
   {
     "lervag/vimtex",
+    lazy = false,
      config = function()
         vim.g.vimtex_view_method = 'zathura'
-        vim.g.latex_view_general_viewer = 'zathura'
-        vim.g.vimtex_compiler_proname = 'latexmk'
-        vim.g.vimtex_quickfix_enabled = 1
-        vim.g.vimtex_syntax_enabled = 1
-        vim.g.vimtex_quickfix_mode = 0
       end,
       ft = 'tex',
   },
